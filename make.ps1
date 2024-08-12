0852df4a653ecd4f5b30f68b3cd727c21e25bb28
Daniel McGuire <146508360+DanielLMcGuire@users.noreply.github.com>
2024-08-12 11:54:08 -0500

    Updated make.ps1

--- make.ps1
+++ make.ps1
@@ -1,12 +1,26 @@
-# Clean up old files
-del .\bin\*.obj -ErrorAction SilentlyContinue
-del .\bin\*.exe -ErrorAction SilentlyContinue
+# Clean up previous build artifacts
+Remove-Item -Path bin\* -Recurse -Force
+Remove-Item -Path bin -Recurse -Force
+mkdir bin\ 
+Write-Host "Cleaned Bin Directory!"
+$destinationPathTextures = "bin\data\txd\"
+New-Item -Path $destinationPathTextures -ItemType Directory -Force
 
-Write-Host "Compiling"
+
+# Copy assets
+Copy-Item -Path levels\demo.level -Destination bin\level.ini
+Copy-Item -Path assets\data\txd\* -Destination $destinationPathTextures -Recurse
+Write-Host "Copied assets"
+
+# Copy DLLs
+Copy-Item -Path 3rdpty\bin\* -Destination bin\ -Recurse
+Write-Host "Copied DLLs"
 
 # Compile game files
+Write-Host Compiling GAME
 cl /EHsc /MP /I".\3rdpty\include" .\game\src\main.cpp .\game\src\debug.cpp /link /LIBPATH:".\3rdpty\lib" User32.lib sfml-graphics.lib sfml-window.lib sfml-system.lib /MACHINE:X86 /OUT:".\bin\game.exe"
 
+Write-Host Compiling Level Viewer
 # Compile level viewer
 cl /EHsc /MP /I"./3rdpty/include" .\tools\src\levelviewer.cpp /link /LIBPATH:"./3rdpty/lib" User32.lib sfml-graphics.lib sfml-window.lib sfml-system.lib /MACHINE:X86 /SUBSYSTEM:WINDOWS /OUT:"./bin/levelviewer.exe"
 
