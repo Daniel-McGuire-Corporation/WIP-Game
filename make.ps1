47b4ed0755e0d31a1992c0b5d008490e4fae237e
Daniel McGuire <146508360+DanielLMcGuire@users.noreply.github.com>
2024-08-13 01:06:41 -0500

    Made script better (use -help)

--- make.ps1
+++ make.ps1
@@ -1,70 +1,88 @@
 param (
-    [switch]$debug
+    [switch]$debug,
+    [switch]$compile,
+    [switch]$Tools,
+    [switch]$Editor,
+    [switch]$Viewer,
+    [switch]$game,
+    [switch]$run,
+    [switch]$all,
+    [switch]$help,
+    [switch]$h
 )
 
-# Clean up previous build artifacts
-Write-Host "Cleaning bin Directories" -ForegroundColor Yellow
-Remove-Item -Path bin\* -Recurse -Force
-Remove-Item -Path bin -Recurse -Force
-Remove-Item -Path tools\bin\* -Recurse -Force
-Remove-Item -Path tools\bin -Recurse -Force
-Write-Host "Cleaned bin Directories." -ForegroundColor Green
-
-Write-Host "Making Directories" -ForegroundColor Yellow
-mkdir bin\ -Force
-mkdir tools\bin\ -Force
-mkdir bin\data\txd\ -Force
-mkdir tools\bin\data\txd\ -Force
-mkdir bin\data\levels -Force
-mkdir tools\bin\data\levels -Force
-Clear
-Write-Host "Made Directories." -ForegroundColor Green
-
-# Copy assets
-Write-Host "Copying assets" -ForegroundColor Yellow
-Copy-Item -Path levels\demo.level -Destination bin\data\levels\level1.ini
-Copy-Item -Path assets\data\txd\* -Destination bin\data\txd -Recurse
-Copy-Item -Path levels\demo.level -Destination tools\bin\data\levels\level1.ini
-Copy-Item -Path assets\data\txd\* -Destination tools\bin\data\txd -Recurse
-Clear
-Write-Host "Copied assets" -ForegroundColor Green
-
-# Copy DLLs
-Write-Host "Copying DLLs" -ForegroundColor Yellow
-Copy-Item -Path 3rdpty\bin\* -Destination bin\ -Recurse
-Copy-Item -Path 3rdpty\bin\* -Destination tools\bin\ -Recurse
-Clear
-Write-Host "Copied DLLs" -ForegroundColor Green
-Clear
-Write-Host "Starting Compilation" -ForegroundColor Yellow
-
-# Compile resources
-Write-Host "Compiling Game Resources" -ForegroundColor Yellow
-rc /fo game_resources.res .\game\ico.rc
-Write-Host "Compiled Game Resources" -ForegroundColor Green
-
-Write-Host "Compiling Level Viewer Resources" -ForegroundColor Yellow
-rc /fo levelview_resources.res .\tools\src\levelview\ico.rc
-Write-Host "Compiled Level Viewer Resources" -ForegroundColor Green
-
-# Compile game files
-Write-Host "Compiling GAME" -ForegroundColor Yellow
-if ($debug) {
-    cl /EHsc /std:c++17 /MP /I".\3rdpty\include" /DDEBUG_BUILD .\game\game\main.cpp .\game\vari.cpp .\game\debug\debug.cpp game_resources.res /link /LIBPATH:".\3rdpty\lib" User32.lib sfml-graphics.lib sfml-window.lib sfml-system.lib /MACHINE:X86 /OUT:".\bin\game-debug.exe"
-} else {
-    cl /EHsc /std:c++17 /MP /I".\3rdpty\include" .\game\game\main.cpp .\game\vari.cpp game_resources.res /link /LIBPATH:".\3rdpty\lib" User32.lib sfml-graphics.lib sfml-window.lib sfml-system.lib /MACHINE:X86 /OUT:".\bin\game.exe"
+function Show-Help {
+    Write-Host "Usage: script.ps1 [options]" -ForegroundColor Cyan
+    Write-Host ""
+    Write-Host "Options:" -ForegroundColor Cyan
+    Write-Host "  -run             Run the specified program(s)" -ForegroundColor Green
+    Write-Host "  -compile         Compile specified targets" -ForegroundColor Green
+    Write-Host "  -Tools           Specify all tools" -ForegroundColor Green
+    Write-Host "  -Editor          Specify the level editor" -ForegroundColor Green
+    Write-Host "  -Viewer          Specify the level viewer" -ForegroundColor Green
+    Write-Host "  -game            Specify the game" -ForegroundColor Green
+    Write-Host "  -all             Specify everything" -ForegroundColor Green
+    Write-Host "  -debug           Specify debug" -ForegroundColor Green
+    Write-Host "  -help, -h        Show this help message" -ForegroundColor Green
 }
-Write-Host "Compiled GAME" -ForegroundColor Green
 
-# Compile tools
-Write-Host "Compiling Tools" -ForegroundColor Yellow
-cl /EHsc /std:c++17 /MP /I"./3rdpty/include" .\tools\src\levelview\levelviewer.cpp .\tools\src\levelview\nfd_common.c .\tools\src\levelview\nfd_win.cpp levelview_resources.res /link /LIBPATH:"./3rdpty/lib" Ole32.lib Shell32.lib User32.lib sfml-graphics.lib Comdlg32.lib sfml-window.lib sfml-system.lib /MACHINE:X86 /SUBSYSTEM:WINDOWS /OUT:"./tools/bin/levelviewer.exe"
-Write-Host "Compiled Level Viewer" -ForegroundColor Green
+if ($help -or $h) {
+    Show-Help
+    exit
+}
 
-Write-Host "Compiling Tools" -ForegroundColor Yellow
+if ($compile) {
+    if ($Tools -or $all) {
+        Write-Host "Compiling level viewer..." -ForegroundColor Yellow
+        rc /fo levelview_resources.res .\tools\src\levelview\ico.rc
+        cl /EHsc /std:c++17 /MP /I"./3rdpty/include" .\tools\src\levelview\levelviewer.cpp .\tools\src\levelview\nfd_common.c .\tools\src\levelview\nfd_win.cpp levelview_resources.res /link /LIBPATH:"./3rdpty/lib" Ole32.lib Shell32.lib User32.lib sfml-graphics.lib Comdlg32.lib sfml-window.lib sfml-system.lib /MACHINE:X86 /SUBSYSTEM:WINDOWS /OUT:"./tools/bin/levelviewer.exe"
+        Write-Host "Compiled Level Viewer" -ForegroundColor Green
 
-Write-Host "Compiled Level Viewer" -ForegroundColor Green
-pip install pyinstaller
-pyinstaller --onefile --windowed --distpath .\tools\bin\ --workpath .\tmp .\tools\src\leveledit\leveleditor.py --icon .\tools\src\leveledit\edit.ico
-Copy-Item -Path tools\src\leveledit\edit.ico -Destination tools\bin\edit.ico
-Write-Host "Compilation Completed!" -ForegroundColor Green
+        Write-Host "Compiling Level Editor" -ForegroundColor Yellow
+        pip install pyinstaller
+        pyinstaller --onefile --windowed --distpath .\tools\bin\ --workpath .\tmp .\tools\src\leveledit\leveleditor.py --icon .\tools\src\leveledit\edit.ico
+        Copy-Item -Path tools\src\leveledit\edit.ico -Destination tools\bin\edit.ico
+        Write-Host "Compiled Level Editor" -ForegroundColor Green
+    }
+    if ($Editor -or $all) {
+        Write-Host "Compiling level editor..." -ForegroundColor Yellow
+        pip install pyinstaller
+        pyinstaller --onefile --windowed --distpath .\tools\bin\ --workpath .\tmp .\tools\src\leveledit\leveleditor.py --icon .\tools\src\leveledit\edit.ico
+        Copy-Item -Path tools\src\leveledit\edit.ico -Destination tools\bin\edit.ico
+        Write-Host "Compiled Level Editor" -ForegroundColor Green
+    }
+    if ($Viewer -or $all) {
+        Write-Host "Compiling level viewer..." -ForegroundColor Yellow
+        rc /fo levelview_resources.res .\tools\src\levelview\ico.rc
+        cl /EHsc /std:c++17 /MP /I"./3rdpty/include" .\tools\src\levelview\levelviewer.cpp .\tools\src\levelview\nfd_common.c .\tools\src\levelview\nfd_win.cpp levelview_resources.res /link /LIBPATH:"./3rdpty/lib" Ole32.lib Shell32.lib User32.lib sfml-graphics.lib Comdlg32.lib sfml-window.lib sfml-system.lib /MACHINE:X86 /SUBSYSTEM:WINDOWS /OUT:"./tools/bin/levelviewer.exe"
+        Write-Host "Compiled Level Viewer" -ForegroundColor Green
+    }
+    if ($game -or $all) {
+        Write-Host "Compiling game..." -ForegroundColor Yellow
+        rc /fo game_resources.res .\game\ico.rc
+        if ($debug) {
+            cl /EHsc /std:c++17 /MP /I".\3rdpty\include" /DDEBUG_BUILD .\game\game\main.cpp .\game\vari.cpp .\game\debug\debug.cpp .\game\ai\enemie.cpp game_resources.res /link /LIBPATH:".\3rdpty\lib" User32.lib sfml-graphics.lib sfml-window.lib sfml-system.lib /MACHINE:X86 /OUT:".\bin\game-debug.exe"
+        } else {
+            cl /EHsc /std:c++17 /MP /I".\3rdpty\include" .\game\game\main.cpp .\game\vari.cpp .\game\ai\enemie.cpp game_resources.res /link /LIBPATH:".\3rdpty\lib" User32.lib sfml-graphics.lib sfml-window.lib sfml-system.lib /MACHINE:X86 /OUT:".\bin\game.exe"
+        }
+        Write-Host "Compiled game" -ForegroundColor Green
+    }
+}
+
+if ($run) {
+    Write-Host "Running the specified program(s)..." -ForegroundColor Yellow
+    if ($Tools -or $all) {
+        Write-Host "Running Level Editor..." -ForegroundColor Green
+        Start-Process -FilePath tools\bin\leveleditor.exe
+        Write-Host "Running Level Viewer..." -ForegroundColor Green
+        Start-Process -FilePath tools\bin\levelviewer.exe
+    }
+    if ($game -or $all) {
+        Write-Host "Running Game..." -ForegroundColor Green
+        if ($debug) {
+            Start-Process -FilePath bin\game-debug.exe
+        } else {
+            Start-Process -FilePath bin\game.exe
+        }
+    }
+}
