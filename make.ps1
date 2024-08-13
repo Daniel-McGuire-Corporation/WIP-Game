c8ec29bece408d528426d3b0c9d83245dea0a129
Daniel McGuire <146508360+DanielLMcGuire@users.noreply.github.com>
2024-08-12 20:01:11 -0500

    Added -debug

--- make.ps1
+++ make.ps1
@@ -1,53 +1,55 @@
+param (
+    [switch]$debug
+)
+
 # Clean up previous build artifacts
-write-host "Cleaning bin Directories" -ForegroundColor Yellow
+Write-Host "Cleaning bin Directories" -ForegroundColor Yellow
 Remove-Item -Path bin\* -Recurse -Force
 Remove-Item -Path bin -Recurse -Force
 Remove-Item -Path tools\bin\* -Recurse -Force
 Remove-Item -Path tools\bin -Recurse -Force
-write-host "Cleaned bin Directories." -ForegroundColor Green
+Write-Host "Cleaned bin Directories." -ForegroundColor Green
 
-write-host "Making Directories" -ForegroundColor Yellow
+Write-Host "Making Directories" -ForegroundColor Yellow
 mkdir bin\ -Force
 mkdir tools\bin\ -Force
 mkdir bin\data\txd\ -Force
 mkdir tools\bin\data\txd\ -Force
 mkdir bin\data\levels -Force
 mkdir tools\bin\data\levels -Force
-clear
-write-host "Made Directories." -ForegroundColor Green
+Clear
+Write-Host "Made Directories." -ForegroundColor Green
 
 # Copy assets
-write-host "Coping assets" -ForegroundColor Yellow
+Write-Host "Copying assets" -ForegroundColor Yellow
 Copy-Item -Path levels\demo.level -Destination bin\data\levels\level1.ini
 Copy-Item -Path assets\data\txd\* -Destination bin\data\txd -Recurse
 Copy-Item -Path levels\demo.level -Destination tools\bin\data\levels\level1.ini
 Copy-Item -Path assets\data\txd\* -Destination tools\bin\data\txd -Recurse
-clear
+Clear
 Write-Host "Copied assets" -ForegroundColor Green
 
 # Copy DLLs
-write-host "Coping DLLs" -ForegroundColor Yellow
+Write-Host "Copying DLLs" -ForegroundColor Yellow
 Copy-Item -Path 3rdpty\bin\* -Destination bin\ -Recurse
 Copy-Item -Path 3rdpty\bin\* -Destination tools\bin\ -Recurse
-clear
+Clear
 Write-Host "Copied DLLs" -ForegroundColor Green
-clear
+Clear
 Write-Host "Starting Compilation" -ForegroundColor Yellow
 
 # Compile game files
 Write-Host "Compiling GAME" -ForegroundColor Yellow
-cl /EHsc /std:c++17 /MP /I".\3rdpty\include" .\game\src\main.cpp .\game\src\debug\debug.cpp /link /LIBPATH:".\3rdpty\lib" User32.lib sfml-graphics.lib sfml-window.lib sfml-system.lib /MACHINE:X86 /OUT:".\bin\game.exe"
+if ($debug) {
+    cl /EHsc /std:c++17 /MP /I".\3rdpty\include" /DDEBUG_BUILD .\game\game\main.cpp .\game\vari.cpp .\game\debug\debug.cpp /link /LIBPATH:".\3rdpty\lib" User32.lib sfml-graphics.lib sfml-window.lib sfml-system.lib /MACHINE:X86 /OUT:".\bin\game-debug.exe"
+} else {
+    cl /EHsc /std:c++17 /MP /I".\3rdpty\include" .\game\game\main.cpp .\game\vari.cpp /link /LIBPATH:".\3rdpty\lib" User32.lib sfml-graphics.lib sfml-window.lib sfml-system.lib /MACHINE:X86 /OUT:".\bin\game.exe"
+}
 Write-Host "Compiled GAME" -ForegroundColor Green
 
+# Compile tools
 Write-Host "Compiling Tools" -ForegroundColor Yellow
-# Compile level viewer
 cl /EHsc /std:c++17 /MP /I"./3rdpty/include" .\tools\src\levelview\levelviewer.cpp .\tools\src\levelview\nfd_common.c .\tools\src\levelview\nfd_win.cpp /link /LIBPATH:"./3rdpty/lib" Ole32.lib Shell32.lib User32.lib sfml-graphics.lib Comdlg32.lib sfml-window.lib sfml-system.lib /MACHINE:X86 /SUBSYSTEM:WINDOWS /OUT:"./tools/bin/levelviewer.exe"
 Write-Host "Compiled Level Viewer" -ForegroundColor Green
-
-#Write-Host "Compiling Level Editor" -ForegroundColor Yellow
-# Compile level editor
-#cl /EHsc /std:c++17 /MP /I"./3rdpty/include" .\tools\src\leveledit\editor.cpp /link /LIBPATH:"./3rdpty/lib" Ole32.lib Shell32.lib User32.lib sfml-graphics.lib Comdlg32.lib sfml-window.lib sfml-system.lib /MACHINE:X86 /SUBSYSTEM:WINDOWS /OUT:"./tools/bin/leveledit.exe"
-
-Write-Host "Compiled Tools" -ForegroundColor Green
 
 Write-Host "Compilation Completed!" -ForegroundColor Green
