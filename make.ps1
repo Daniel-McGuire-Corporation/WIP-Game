bccc19ed7ce41a99cfa276dfed641ad4a6ccd23e
Daniel McGuire <146508360+DanielLMcGuire@users.noreply.github.com>
2024-08-13 01:42:47 -0500

    Added back clean to script

--- make.ps1
+++ make.ps1
@@ -15,15 +15,15 @@ function Show-Help {
     Write-Host "Usage: script.ps1 [options]" -ForegroundColor Cyan
     Write-Host ""
     Write-Host "Options:" -ForegroundColor Cyan
-    Write-Host "  -run             Run the specified program(s)" -ForegroundColor Green
-    Write-Host "  -compile         Compile specified targets" -ForegroundColor Green
-    Write-Host "  -Tools           Specify all tools" -ForegroundColor Green
-    Write-Host "  -Editor          Specify the level editor" -ForegroundColor Green
-    Write-Host "  -Viewer          Specify the level viewer" -ForegroundColor Green
-    Write-Host "  -game            Specify the game" -ForegroundColor Green
-    Write-Host "  -all             Specify everything" -ForegroundColor Green
-    Write-Host "  -debug           Specify debug" -ForegroundColor Green
-    Write-Host "  -help, -h        Show this help message" -ForegroundColor Green
+    Write-Host "  -run             Run the specified program(s)"
+    Write-Host "  -compile         Compile specified targets"
+    Write-Host "  -Tools           Specify all tools"
+    Write-Host "  -Editor          Specify the level editor"
+    Write-Host "  -Viewer          Specify the level viewer"
+    Write-Host "  -game            Specify the game"
+    Write-Host "  -all             Specify everything"
+    Write-Host "  -debug           Specify debug"
+    Write-Host "  -help, -h        Show this help message"
 }
 
 if ($help -or $h) {
@@ -32,6 +32,35 @@ if ($help -or $h) {
 }
 
 if ($compile) {
+	Write-Host "Cleaning bin Directories" -ForegroundColor Yellow
+	Remove-Item -Path bin\* -Recurse -Force
+	Remove-Item -Path bin -Recurse -Force
+	Remove-Item -Path tools\bin\* -Recurse -Force
+	Remove-Item -Path tools\bin -Recurse -Force
+	Write-Host "Cleaned bin Directories." -ForegroundColor Green
+	Write-Host "Making Directories" -ForegroundColor Yellow
+	mkdir bin\ -Force
+	mkdir tools\bin\ -Force
+	mkdir bin\data\txd\ -Force
+	mkdir tools\bin\data\txd\ -Force
+	mkdir bin\data\levels -Force
+	mkdir tools\bin\data\levels -Force
+	Write-Host "Made Directories." -ForegroundColor Green
+	Write-Host "Copying assets" -ForegroundColor Yellow
+	Copy-Item -Path levels\demo.level -Destination bin\data\levels\level1.ini
+	Write-Host copied level1.ini
+	Copy-Item -Path assets\data\txd\* -Destination bin\data\txd -Recurse
+	Write-Host copied txd
+	Copy-Item -Path levels\demo.level -Destination tools\bin\data\levels\level1.ini
+	Write-Host copied level1.ini
+	Copy-Item -Path assets\data\txd\* -Destination tools\bin\data\txd -Recurse
+	Write-host copied txds
+	Write-Host "Copied assets" -ForegroundColor Green
+	Write-Host "Copying DLLs" -ForegroundColor Yellow
+	Copy-Item -Path 3rdpty\bin\* -Destination bin\ -Recurse
+	Copy-Item -Path 3rdpty\bin\* -Destination tools\bin\ -Recurse
+	Write-Host "Copied DLLs" -ForegroundColor Green
+    Clear-Host
     if ($Tools -or $all) {
         Write-Host "Compiling level viewer..." -ForegroundColor Yellow
         rc /fo levelview_resources.res .\tools\src\levelview\ico.rc
@@ -71,18 +100,20 @@ if ($compile) {
 
 if ($run) {
     Write-Host "Running the specified program(s)..." -ForegroundColor Yellow
+
     if ($Tools -or $all) {
         Write-Host "Running Level Editor..." -ForegroundColor Green
-        Start-Process -FilePath tools\bin\leveleditor.exe
+        Start-Process -FilePath tools\bin\leveleditor.exe -WorkingDirectory tools\bin
         Write-Host "Running Level Viewer..." -ForegroundColor Green
-        Start-Process -FilePath tools\bin\levelviewer.exe
+        Start-Process -FilePath tools\bin\levelviewer.exe -WorkingDirectory tools\bin
     }
+
     if ($game -or $all) {
         Write-Host "Running Game..." -ForegroundColor Green
         if ($debug) {
-            Start-Process -FilePath bin\game-debug.exe
+            Start-Process -FilePath bin\game-debug.exe -WorkingDirectory bin
         } else {
-            Start-Process -FilePath bin\game.exe
+            Start-Process -FilePath bin\game.exe -WorkingDirectory bin
         }
     }
 }
