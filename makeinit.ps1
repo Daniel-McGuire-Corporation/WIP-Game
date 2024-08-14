98d791c720269c503fd2f2dc40b1bc4553dd004a
Daniel McGuire <146508360+DanielLMcGuire@users.noreply.github.com>
2024-08-14 01:04:05 -0500

    Update makeinit.ps1

--- makeinit.ps1
+++ makeinit.ps1
@@ -1,5 +1,8 @@
 Install-Module ps2exe
 clear-host
-ps2exe .\tools\src\make\make.ps1 .\make.exe | grep Reading
+ps2exe .\tools\src\make\make.ps1 .\tools\bin\make.exe | grep --------
 Clear-Host
-Write-Host Compiled MAKE.EXE
+Write-Host Make has been initialized.
+Write-Host ""
+Write-Host "If you haven't yet, run"
+Write-Host "make -setupengine"
