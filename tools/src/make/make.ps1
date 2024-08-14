e3dfe210e959a01a17bf8ab42d24c857759f8c3e
Daniel McGuire <146508360+DanielLMcGuire@users.noreply.github.com>
2024-08-14 00:38:37 -0500

    Update make.ps1

--- tools/src/make/make.ps1
+++ tools/src/make/make.ps1
@@ -177,17 +177,17 @@ if ($compile) {
     }
     if ($viewer -or $tools -or $all) {
         Write-Host "Compiling level viewer..." -ForegroundColor Yellow
-        rc /fo levelview_resources.res .\tools\src\levelview\ico.rc
-        cl /EHsc /std:c++17 /MP /I"./3rdpty/include" .\tools\src\levelview\levelviewer.cpp .\tools\src\levelview\nfd_common.c .\tools\src\levelview\nfd_win.cpp levelview_resources.res /link /LIBPATH:"./3rdpty/lib" Ole32.lib Shell32.lib User32.lib sfml-graphics.lib Comdlg32.lib sfml-window.lib sfml-system.lib /MACHINE:X86 /SUBSYSTEM:WINDOWS /OUT:"./tools/bin/levelviewer.exe"
+        rc /nologo /fo levelview_resources.res .\tools\src\levelview\ico.rc
+        cl /EHsc /nologo /std:c++17 /MP /I"./3rdpty/include" .\tools\src\levelview\levelviewer.cpp .\tools\src\levelview\nfd_common.c .\tools\src\levelview\nfd_win.cpp levelview_resources.res /link /LIBPATH:"./3rdpty/lib" Ole32.lib Shell32.lib User32.lib sfml-graphics.lib Comdlg32.lib sfml-window.lib sfml-system.lib /MACHINE:X86 /SUBSYSTEM:WINDOWS /OUT:"./tools/bin/levelviewer.exe"
         Write-Host "Compiled Level Viewer" -ForegroundColor Green
     }
     if ($game -or $all) {
         Write-Host "Compiling game..." -ForegroundColor Yellow
-        rc /fo game_resources.res .\game\ico.rc
+        rc /nologo /fo game_resources.res .\game\ico.rc
         if ($debug) {
-            cl /EHsc /std:c++17 /MP /I".\3rdpty\include" /DDEBUG_BUILD .\game\game\main.cpp .\game\vari.cpp .\game\debug\debug.cpp .\game\ai\enemy.cpp game_resources.res /link /LIBPATH:".\3rdpty\lib" User32.lib sfml-graphics.lib sfml-window.lib sfml-system.lib /MACHINE:X86 /OUT:".\bin\game-debug.exe"
+            cl /EHsc /nologo /std:c++17 /MP /I".\3rdpty\include" /DDEBUG_BUILD .\game\game\main.cpp .\game\vari.cpp .\game\debug\debug.cpp .\game\ai\enemy.cpp game_resources.res /link /LIBPATH:".\3rdpty\lib" User32.lib sfml-graphics.lib sfml-window.lib sfml-system.lib /MACHINE:X86 /OUT:".\bin\game-debug.exe"
         } else {
-            cl /EHsc /std:c++17 /MP /I".\3rdpty\include" .\game\game\main.cpp .\game\vari.cpp .\game\ai\enemie.cpp game_resources.res /link /LIBPATH:".\3rdpty\lib" User32.lib sfml-graphics.lib sfml-window.lib sfml-system.lib /MACHINE:X86 /OUT:".\bin\game.exe"
+            cl /EHsc /nologo /std:c++17 /MP /I".\3rdpty\include" .\game\game\main.cpp .\game\vari.cpp .\game\ai\enemie.cpp game_resources.res /link /LIBPATH:".\3rdpty\lib" User32.lib sfml-graphics.lib sfml-window.lib sfml-system.lib /MACHINE:X86 /OUT:".\bin\game.exe"
         }
         Write-Host "Compiled game" -ForegroundColor Green
     }
